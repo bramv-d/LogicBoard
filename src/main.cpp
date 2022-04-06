9e73ffb349efaf9a67a81863afc4f19e8cfe49d7
Dieuwert <dieuwertwolters99@gmail.com>
2022-04-06 16:59:11 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include "main.h"
 
-InputVariable input(0, 1, 2, 2);
+InputVariable row1(0, 1, 2, 2); //Analog input, analog logic, analog compare, digital output
+InputVariable row2(3, 4, 5, 3);
+InputVariable row3(6, 7, 8, 4);
+InputVariable row4(9, 10, 11, 5);
+
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  input.update();
+  row1.update();
+  row2.update();
+  row3.update();
+  row4.update();
+  
 }
