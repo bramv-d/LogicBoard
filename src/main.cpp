0bc41e1e9e932d56b971e1e289e8f5dfa2e17595
Bram van Driel <bramjvandriel@gmail.com>
2022-04-06 16:32:49 +0200

    Hoi

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 
 InputVariable input(0, 1, 2, 2);
 void setup() {
-  // put your setup code here to run once:
   Serial.begin(9600);
-  // put your setup code here,dwadweawdawda to run once:
 }
 
 void loop() {
